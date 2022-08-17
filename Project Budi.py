2c93411b2b8db6267bae99fdc0de4f1965f4b4da
WINDOWS <enktrans@gmail.com>
2022-08-17 22:52:07 +0700

    Budi beli telur dan susu

---
+++ Project Budi.py
@@ -0,0 +1,30 @@
+# TUGAS DASAR BUDI
+# SEQUENSIAL : Eksekusi secara Berurutan
+
+print('"Ibu berkata : beli 1 botol susu"')
+print('Ibu berkata lagi : "Jika ada telur, beli 5 telur"')
+print('anak menjawab: "ok"')
+
+print('anak pergi ke toko')
+
+#PERCABANGAN :
+print('Anak : "apakah ada susu?"')
+tidak = False
+if tidak:
+    print('anak tidak jadi beli susu')
+else:
+    print('anak mengecek hrganya, cukup')
+    print('anak beli 1 botol susu')
+
+print('"apakah ada telur?"')
+tidak: False
+if tidak:
+    print('anak tidak beli telur, hanya beli susu')
+else:
+    print('anak mengecek harganya, cukup')
+    print('anak beli 5 butir telur')
+
+print('anak pulang kerumah')
+print('menyerahkan hasil belanjanya kepada ibu')
+print('selesai')
+
