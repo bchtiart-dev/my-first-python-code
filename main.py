9b9f75b237267011f40dcb878a7b93b81ac3f7e6
WINDOWS <enktrans@gmail.com>
2022-08-20 15:08:04 +0700

    telur dan susu

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ Ini adalah project pertama dengan Python
 
 print("hello word")
 print("my name is bchtiart")
+
+
+# Percabangan
+milk_bottle_count = 354
+egg_count = 87654
+print(f"Jumlah botol susu {milk_bottle_count}")
+print(f"Jumlah telur {egg_count}")
+
+if milk_bottle_count > 0:
+    print("Budi membeli 1 botol susu")
+    if egg_count == 0:
+        print("budi membeli 1 botol susu")
+else:
+    print("Budi tidak membeli 1 botol susu")
