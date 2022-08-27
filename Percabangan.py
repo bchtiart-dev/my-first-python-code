6e7a598c324e8ebc0715ab1aad78afeb77eb81a1
WINDOWS <enktrans@gmail.com>
2022-08-27 09:20:04 +0700

    percabangan telur dan susu

---
+++ Percabangan.py
@@ -0,0 +1,23 @@
+# PERCABANGAN
+
+jumlah_botol_susu = 8
+jumlah_telur = 8
+
+print('ibu menyuruh budi ke toko "Budi, beli susu 1 botol"')
+print('Budi menjawab"iya Bu"')
+
+print('kalau ada telur, beli 5')
+print('siap. Budi berangkat ke Toko')
+
+if jumlah_botol_susu > 0:
+    print('budi mengecek harganya, cukup')
+    print('budi membeli 1 botol susu')
+else:
+    print('budi tidak membeli 1 botol susu')
+
+
+print('budi melihat ada telur')
+if jumlah_telur > 0:
+    print('budi membeli 5 telur')
+else:
+    print('Budi tidak membeli telur, karena tidak ada')
