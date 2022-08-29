5a8f6188a53eb1c63dfc4d452f4cb5d1442c11b0
WINDOWS <enktrans@gmail.com>
2022-08-29 20:03:42 +0700

    rename in english

---
+++ Perulangan_membaca_buku_dengan_while_undefind_count.py
@@ -0,0 +1,24 @@
+"""
+Program perulangan membaca buku sampai paham
+"""
+book_count = 10
+print('Ibu berkata "baca semua buku"')
+
+understood_count = 0
+print(f'jumlah buku yang sudah dibaca dan dipahami {understood_count}')
+
+read_count = 0
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f'Buku ke {understood_count + 1} belum paham')
+    else:
+        understood_count = understood_count + 1
+        print(f'buku ke {understood_count} sudah dibaca dan dipahami')
+
+print(f'Jumlah buku yang sudah dibaca dan dipahami {understood_count}')
+
+if understood_count == book_count:
+    print('"Bu, semua buku sudah dibaca"')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. Budi hanya bisa memahami {understood_count} buku"')
